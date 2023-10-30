c7ea14c5eb37b35f0ab436d2bf03c2aa5e9d7ce9
Rebecca Turner <rbt@sent.as>
2023-10-30 13:53:33 -0400

    Package with crane (#17)
    Also add Garnix CI.
    
    Fixes #9.
    Fixes #16.

--- flake.nix
+++ flake.nix
@@ -4,18 +4,56 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     systems.url = "github:nix-systems/default";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = {
+  nixConfig = {
+    extra-substituters = ["https://cache.garnix.io"];
+    extra-trusted-substituters = ["https://cache.garnix.io"];
+    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
+
+  outputs = inputs @ {
     self,
     nixpkgs,
     systems,
+    crane,
+    advisory-db,
   }: let
-    eachSystem = function: nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
+    eachSystem = function:
+      nixpkgs.lib.genAttrs
+      (import systems)
+      (system: function nixpkgs.legacyPackages.${system});
   in {
     lib =
       eachSystem
       (pkgs:
         pkgs.callPackage ./nix/makeLib.nix {});
+
+    packages = eachSystem (pkgs: let
+      inherit (pkgs) lib;
+      packages = pkgs.callPackage ./nix/makePackages.nix {inherit inputs;};
+    in
+      (lib.filterAttrs (name: value: lib.isDerivation value) packages)
+      // {
+        default = packages.home-mangler;
+      });
+
+    devShells = eachSystem (pkgs: {
+      default = self.packages.${pkgs.system}.home-mangler.devShell;
+    });
+
+    overlays.default = final: prev: let
+      packages = final.callPackage ./nix/makePackages.nix {inherit inputs;};
+    in {
+      inherit (packages) home-mangler;
+    };
   };
 }
---
+++ nix/packages/home-mangler.nix
@@ -0,0 +1,87 @@
+{
+  system,
+  lib,
+  stdenv,
+  libiconv,
+  darwin,
+  inputs,
+  rustPlatform,
+  rust-analyzer,
+}: let
+  inherit (inputs) crane advisory-db;
+  craneLib = crane.lib.${system};
+
+  commonArgs' = {
+    src = craneLib.cleanCargoSource (craneLib.path ../../.);
+
+    nativeBuildInputs = lib.optionals stdenv.isDarwin [
+      # Additional darwin specific inputs can be set here
+      (libiconv.override {
+        enableStatic = true;
+        enableShared = false;
+      })
+      darwin.apple_sdk.frameworks.CoreServices
+    ];
+  };
+
+  # Build *just* the cargo dependencies, so we can reuse
+  # all of that work (e.g. via cachix) when running in CI
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs';
+
+  commonArgs =
+    commonArgs'
+    // {
+      inherit cargoArtifacts;
+    };
+
+  checks = {
+    home-mangler-tests = craneLib.cargoNextest (commonArgs
+      // {
+        NEXTEST_PROFILE = "ci";
+        NEXTEST_HIDE_PROGRESS_BAR = "true";
+      });
+    home-mangler-clippy = craneLib.cargoClippy (commonArgs
+      // {
+        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+      });
+    home-mangler-doc = craneLib.cargoDoc (commonArgs
+      // {
+        cargoDocExtraArgs = "--document-private-items";
+        RUSTDOCFLAGS = "-D warnings";
+      });
+    home-mangler-fmt = craneLib.cargoFmt commonArgs;
+    home-mangler-audit = craneLib.cargoAudit (commonArgs
+      // {
+        inherit advisory-db;
+      });
+  };
+
+  devShell = craneLib.devShell {
+    inherit checks;
+
+    # Make rust-analyzer work
+    RUST_SRC_PATH = rustPlatform.rustLibSrc;
+
+    # Extra development tools (cargo and rustc are included by default).
+    packages = [
+      rust-analyzer
+    ];
+  };
+in
+  # Build the actual crate itself, reusing the dependency
+  # artifacts from above.
+  craneLib.buildPackage (commonArgs
+    // {
+      # Don't run tests; we'll do that in a separate derivation.
+      # This will allow people to install and depend on `home-mangler`
+      # without downloading a half dozen different versions of GHC.
+      doCheck = false;
+
+      # Only build `home-mangler`, not the test macros.
+      cargoBuildCommand = "cargoWithProfile build";
+
+      passthru = {
+        inherit checks;
+        inherit devShell;
+      };
+    })
