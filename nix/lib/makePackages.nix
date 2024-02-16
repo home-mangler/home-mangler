270a20f46a8a0e5977d1f11672bdae87fc8dd1a5
Rebecca Turner <rbt@sent.as>
2024-02-16 13:03:29 -0500

    Install `outputsToInstall` for all packages (#30)

--- nix/lib/makePackages.nix
+++ nix/lib/makePackages.nix
@@ -1,8 +1,24 @@
-{symlinkJoin}:
-# TODO: Docs for `symlinkJoin` say it might not "work right" if inputs have
-# symlinks to directories. Investigate / mitigate?
-packages:
-symlinkJoin {
-  name = "home-mangler-packages";
-  paths = packages;
-}
+{
+  lib,
+  symlinkJoin,
+}: packages: let
+  allOutputs =
+    lib.flatten
+    (builtins.map
+      (
+        pkg:
+          if pkg ? meta && pkg.meta ? outputsToInstall
+          # If `outputsToInstall` is specified, use that.
+          then builtins.map (output: pkg.${output}) pkg.meta.outputsToInstall
+          # Otherwise, install the default output and the `doc` and `man`
+          # outputs, if they exist.
+          else [pkg] ++ lib.optional (pkg ? doc) pkg.doc ++ lib.optional (pkg ? man) pkg.man
+      )
+      packages);
+in
+  # TODO: Docs for `symlinkJoin` say it might not "work right" if inputs have
+  # symlinks to directories. Investigate / mitigate?
+  symlinkJoin {
+    name = "home-mangler-packages";
+    paths = allOutputs;
+  }
